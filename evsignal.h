c1325989e9d6c7c2a984790bc4212d6a2910dccf
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-10-06 23:24:55 +0200

    Remove #ifdef HAVE_SIGACTION form code.

--- evsignal.h
+++ evsignal.h
@@ -29,8 +29,6 @@
 #ifndef _EVSIGNAL_H_
 #define _EVSIGNAL_H_
 
-typedef void (*ev_sighandler_t)(int);
-
 struct evsignal_info {
 	struct event ev_signal;
 	int ev_signal_pair[2];
@@ -38,11 +36,7 @@ struct evsignal_info {
 	volatile sig_atomic_t evsignal_caught;
 	struct event_list evsigevents[NSIG];
 	sig_atomic_t evsigcaught[NSIG];
-#ifdef HAVE_SIGACTION
 	struct sigaction **sh_old;
-#else
-	ev_sighandler_t **sh_old;
-#endif
 	int sh_old_max;
 };
 int evsignal_init(struct event_base *);
