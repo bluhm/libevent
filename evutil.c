10e86cd89268c540c8c7eb8f4027bb45a2f8950c
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-10-18 19:43:38 +0200

    Remove #ifdef _MSC_VER.

--- evutil.c
+++ evutil.c
@@ -85,18 +85,9 @@ evutil_snprintf(char *buf, size_t buflen, const char *format, ...)
 int
 evutil_vsnprintf(char *buf, size_t buflen, const char *format, va_list ap)
 {
-#ifdef _MSC_VER
-	int r = _vsnprintf(buf, buflen, format, ap);
-	buf[buflen-1] = '\0';
-	if (r >= 0)
-		return r;
-	else
-		return _vscprintf(format, ap);
-#else
 	int r = vsnprintf(buf, buflen, format, ap);
 	buf[buflen-1] = '\0';
 	return r;
-#endif
 }
 
 static int
