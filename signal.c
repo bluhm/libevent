6ad13d015fe90b5b7a010add4e4e68c59680bb16
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-01-06 12:43:04 +0100

    cvs commit

--- signal.c
+++ signal.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: signal.c,v 1.24 2014/10/30 16:45:37 bluhm Exp $	*/
+/*	$OpenBSD: signal.c,v 1.25 2015/01/06 11:42:37 bluhm Exp $	*/
 
 /*
  * Copyright 2000-2002 Niels Provos <provos@citi.umich.edu>
