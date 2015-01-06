cba3088e30c1e726c0e2a1da55c02a0117536266
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-01-07 00:11:31 +0100

    cvs commit

--- event.c
+++ event.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: event.c,v 1.37 2015/01/06 11:27:35 bluhm Exp $	*/
+/*	$OpenBSD: event.c,v 1.38 2015/01/06 23:11:23 bluhm Exp $	*/
 
 /*
  * Copyright (c) 2000-2004 Niels Provos <provos@citi.umich.edu>
